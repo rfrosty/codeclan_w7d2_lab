8fa7ab6d36c3ad6df3aae73873810d3c1329e252
EismanW <pl.amosso@gmail.com>
2019-11-26 13:53:43 +0000

    fetched countries and added calculated field

--- src/app.js
+++ src/app.js
@@ -4,10 +4,23 @@ document.addEventListener("DOMContentLoaded", () => {
     new Vue({
         el: "#app",
         data: {
-
+            countries: []
+        },
+        computed: {
+            totalPopulation: function() {
+                return 0;
+            }
+        },
+        mounted() {
+            this.fetchCountries();
         },
         methods: {
-            
+            fetchCountries: function() {
+                fetch('https://restcountries.eu/rest/v2/all')
+                .then(response => response.json())
+                .then(data => this.countries = data)
+                .catch(console.log("Error while trying to fetch countries"));
+            }
         }
     });
 });
